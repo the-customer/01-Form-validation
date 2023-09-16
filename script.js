0e242e3991b680634ec4b268b725f6154ddc552e
the-customer <niangalytall@gmail.com>
2023-09-16 19:54:28 +0000

    Cours 03 Form Validation Fin

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const photo = document.getElementById('photo');
 //recuperer le bouton
 const btn = document.querySelector('button');
 //recuperer le span
@@ -10,25 +11,65 @@ const span = document.querySelector('span');
 
 
 //Fonctions
-function validate(inputTab){
+function checkRequired(inputTab){
     inputTab.forEach(function(input){
-        //Recuperer la div mere:
-        const divParent = input.parentNode;
         if(input.value.length == 0){
-            input.style.border = '2px solid red';
-            //afficher le message d'erreur
-            divParent.classList.remove('success');
-            divParent.classList.add('error');
+            error(input,'Le champs est requis');  
         }else{
-            input.style.border = '2px solid green';
-            //cacher le message d'erreur
-            divParent.classList.add('success');
-            divParent.classList.remove('error');
+            success(input);
         }
     });
+}
+//
+function samePassword(input1,input2){
+    if(input1.value != input2.value){
+        error(input2,'Les deux mots de passes ne sont pas identiques');
+    }
+}
+//
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        error(input,'Il faut au minimum '+min+' caracteres');
+    }else if(input.value.length > max){
+        error(input,'Il faut au maximum '+max+' caracteres');
+    }else{
+        success(input);
+    }
+}
+//
+function error(input,message='Error'){
+    //Recuperer la div mere:
+    const divParent = input.parentNode;
+    input.style.border = '2px solid red';
+    divParent.classList.remove('success');
+    divParent.classList.add('error');
+    //
+    const small = divParent.querySelector('small');
+    small.innerText = message;
+}
+//
+function success(input){
+    //Recuperer la div mere:
+    const divParent = input.parentNode;
+    input.style.border = '2px solid green';
+    divParent.classList.add('success');
+    divParent.classList.remove('error');
 }
 
 //Evenements
 btn.addEventListener('click',function(){
-    validate([username,email,password,password2]);
+    checkRequired([username,email,password,password2]);
+    samePassword(password,password2);
+    checkLength(username,3,15);
+    checkLength(password,6,25);
 });
+//
+photo.addEventListener('change',function(){
+    const photo = document.getElementById('photo');
+    const f = new FileReader();
+    f.readAsDataURL(photo.files[0]);
+    f.onloadend = function(event){
+        const path = event.target.result;
+        document.getElementById('img').setAttribute('src',path); //afficher l'image choisi
+    }
+})
